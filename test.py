f6c2222c14a6c7a6dab09cf18e2c16a61b381848
Nathan Edwards <edwardsnj@users.noreply.github.com>
2022-12-12 22:08:01 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+#!/bin/env python3
+import sys
+
+print("Test started",file=sys.stderr)
+
 import max3100
 
 ser = max3100.MAX3100()
@@ -6,3 +11,4 @@ ser.open(0,0,2,9600)
 
 ser.close()
 
+print("Test finished",file=sys.stderr)
